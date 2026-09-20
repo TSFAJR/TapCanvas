2136d47b0f756a00e99c90f05b5ec9b465c7b8bd
李碧强 <624144061@qq.com>
2026-09-20 19:29:34 +0800

    build(web): update production bundle for unified AI chat actions

---
+++ apps/web/dist/assets/PortalRuntime-oH6cJa47.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-react-DwcYshSI.js";import{P as o}from"./RouteEntrypoint-C316jk0V.js";import{M as i}from"./MantineRuntimeProvider-Bfkm9nyz.js";import"./index-B7tLhfW-.js";import"./vendor-mantine-D9W4AfLB.js";function p({route:t}){return r.jsx(i,{children:r.jsx(o,{route:t})})}export{p as default};
